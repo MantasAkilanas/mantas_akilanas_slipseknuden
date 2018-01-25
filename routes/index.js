98b89033c8f9bb3a1eefb718f4ace6e7e6a93ff5
MantasAkilanas <4294018@rts-365.dk>
2018-01-25 21:13:52 +0100

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
 module.exports = (app) => {
     app.get("/", (req, res) => {
-        res.render("pages/index");
+        res.render("pages/index", {
+            sidebar: [{
+                title: "SLIPSKNUDEN ONLINE.",
+                text: "VI LANCERER NU VORES HJEMMESIDEN MED BILLEDER AF VORES ALLER FINESTE SLIPS."
+            }, {
+                title: "SLIPSKNUDEN ONLINE.",
+                text: "VI LANCERER NU VORES HJEMMESIDEN MED BILLEDER AF VORES ALLER FINESTE SLIPS."
+            }],
+            frontpageProducts: [{
+                name: "Sleek Fabric UK",
+                text: "Sort nat, slipset til den ultra stilbeviste",
+                pris: 900,
+                billede:"1_klassisk_brun.jpg"
+            }, {
+                name: "Sleek Fabric UK",
+                text: "Sort nat, slipset til den ultra stilbeviste",
+                pris: 900,
+                billede: "2_stribet_stil.jpg"
+            }]
+        })
     })
 }
